c93ed16c19eb5a08a575ba5cef63af53d2970f6e
EC2 Default User <ec2-user@ip-172-31-7-4.ec2.internal>
2018-08-22 02:28:13 +0000

    signup

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -24,6 +24,7 @@
                             </ul>
                         </li>
                     @else
+                        <li>{!! link_to_route('signup.get', '管理者登録') !!}</li>
                         <li>{!! link_to_route('login', 'ログイン') !!}</li>
                     @endif
                 </ul>
